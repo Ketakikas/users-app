659608847df0c78b2a139b8a45579c997a277b24
Ketaki Kashyap <KetakiK2@citiustech.com>
2022-01-12 11:29:37 +0530

    new componenets added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import {UsersComponent} from './components/users/users.component';
+import { UserImageComponent } from './components/users/user-image/user-image.component';
+import { UserInfoComponent } from './components/users/user-info/user-info.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsersComponent
+    UsersComponent,
+    UserImageComponent,
+    UserInfoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -17,5 +17,18 @@ import {Component, ViewEncapsulation} from '@angular/core';
     encapsulation:ViewEncapsulation.Emulated
 })
 export class UsersComponent{
-
+    user={
+        pic:"https://cdn.britannica.com/47/188747-050-1D34E743/Bill-Gates-2011.jpg",
+        firstname:"Bill",
+        lastname:"Gates",
+        dob:new Date("Dec 21, 1965"),
+        isWorking:true,
+        income:10000,
+        company:"Microsoft",
+        hobbies:["Gaming","Dancing","Studying","Reading"],
+        votes:100
+    }
+    onMoreInfo(user){
+        alert(`${user.firstname} ${user.lastname} with votes ${user.votes}`);
+    }
 }
