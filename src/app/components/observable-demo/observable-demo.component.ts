6fc40944c1bead3b8f2633585050d0a4f821946b
Ketaki Kashyap <KetakiK2@citiustech.com>
2022-01-19 15:00:36 +0530

    fromEvent observable

--- src/app/components/observable-demo/observable-demo.component.ts
+++ src/app/components/observable-demo/observable-demo.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Button } from 'protractor';
 import {from, interval, of, Subscription,fromEvent} from 'rxjs';
 
@@ -7,22 +7,54 @@ import {from, interval, of, Subscription,fromEvent} from 'rxjs';
   templateUrl: './observable-demo.component.html',
   styleUrls: ['./observable-demo.component.css']
 })
-export class ObservableDemoComponent {
+export class ObservableDemoComponent implements AfterViewInit{
+  
   //by convention, use $ at the end of name which holds the observable value
+  @ViewChild('btnObservable') //viewchild refers the template controls
+  btn :ElementRef<any>;
+  
+  @ViewChild("txtName")
+  txtName:ElementRef<any>;
+
   source$=interval(1000);//after 1 second, it will emit a value
+  
   fruits=["apple","banana","orange","grapes"];
+  
   source2$=from(this.fruits); //observable from collection
+  
   source3$=of("apple",101,true,"Hello World"); //observable from any type
+  
   unSub$:Subscription;
+  
   source4$=fromEvent(document,"click");
-  constructor() { }
+  
+  constructor() { 
+    this.unSub$=this.source4$.subscribe((eventData)=>{
+      console.log("document click",eventData);
+    });
+  }
+  
+  //ngAfterViewInit loads after view or template have been initialized
+  ngAfterViewInit(): void {
+    //observable after click event on button element
+    let source6$=fromEvent(this.btn.nativeElement,"click");
+    source6$.subscribe((eventData)=>{
+      console.log("button click",eventData);
+    });
+    //observable after keyup event on input element
+    let source7$=fromEvent(this.txtName.nativeElement,"keyup");
+    source7$.subscribe((eData)=>{
+      console.log("KeyUp of Input",eData);
+    });
+  }
+  
   onSubscribe(){
     //subscribe methods consumes the observable value.
-    // this.unSub$=this.source$.subscribe({
-    //   next:data=>{console.log(data)},//called everytime when interval method emit any data
-    //   error:err=>{},//handles error occured while data emission
-    //   complete:()=>{console.log("Completed")}
-    // });
+    this.unSub$=this.source$.subscribe({
+      next:data=>{console.log(data)},//called everytime when interval method emit any data
+      error:err=>{},//handles error occured while data emission
+      complete:()=>{console.log("Completed")}
+    });
     // this.source2$.subscribe({
     //   next:data=>{console.log(data)},//called everytime when interval method emit any data
     //   error:err=>{},//handles error occured while data emission
@@ -33,12 +65,13 @@ export class ObservableDemoComponent {
     //   error:err=>{},//handles error occured while data emission
     //   complete:()=>{console.log("Completed")}
     // });
-    this.unSub$=this.source4$.subscribe({
-      next:data=>{console.log(data)},//called everytime when interval method emit any data
-      error:err=>{},//handles error occured while data emission
-      complete:()=>{console.log("Completed")}
-    });
+    // this.unSub$=this.source4$.subscribe({
+    //   next:data=>{console.log(data)},//called everytime when interval method emit any data
+    //   error:err=>{},//handles error occured while data emission
+    //   complete:()=>{console.log("Completed")}
+    // });
   }
+  
   onUnsubscribe(){
     console.log("Unsubscribed");
       this.unSub$.unsubscribe();
